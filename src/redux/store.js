6f9a4c2f6395f62e9a275fd50631644ff40ed4d2
JaRcomGitHub <jarcom@ukr.net>
2022-11-23 20:47:57 +0200

    add persist, example

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,53 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { createStore } from "redux";
+// import { devToolsEnhancer } from "@redux-devtools/extension";
 
-const initialState = {
-  tasks: [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ],
-  filters: {
-    status: "all",
+// const initialState = {
+//   tasks: [
+//     { id: 0, text: "Learn HTML and CSS", completed: true },
+//     { id: 1, text: "Get good at JavaScript", completed: true },
+//     { id: 2, text: "Master React", completed: false },
+//     { id: 3, text: "Discover Redux", completed: false },
+//     { id: 4, text: "Build amazing apps", completed: false },
+//   ],
+//   filters: {
+//     status: "all",
+//   },
+// };
+
+// const rootReducer = (state = initialState, action) => {
+//   return state;
+// };
+
+// const enhancer = devToolsEnhancer();
+
+// export const store = createStore(rootReducer, enhancer);
+
+
+import { configureStore } from "@reduxjs/toolkit";
+import { getDefaultNormalizer } from "@testing-library/react";
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { clicksReducer } from './clicksSlice';
+
+export const store = configureStore({
+  reducer: {
+    click: clicksReducer,
   },
-};
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE,
+          PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
 
-const rootReducer = (state = initialState, action) => {
-  return state;
-};
-
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+export const persistor = persistStore(store);
